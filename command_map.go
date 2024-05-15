da7e23a600898613b831a895c9465e53189d704c
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-05-15 17:59:48 +0100

    finished map functions

--- command_map.go
+++ command_map.go
@@ -1,14 +1,43 @@
 package main
 
-func commandLocation() {
-
-}
+import (
+	"errors"
+	"fmt"
+)
 
 func commandMap(cfg *config) error {
+	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 
+	if err != nil {
+		return err
+	}
+	cfg.nextLocationsURL = locationResp.Next
+	cfg.prevLocationsURL = locationResp.Previous
+
+	for _, loc := range locationResp.Results {
+		fmt.Println(loc.Name)
+	}
 	return nil
 }
 
 func commandMapb(cfg *config) error {
+
+	if cfg.prevLocationsURL == nil {
+		return errors.New("you are on the first page")
+	}
+
+	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
+
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationResp.Next
+	cfg.prevLocationsURL = locationResp.Previous
+
+	for _, loc := range locationResp.Results {
+		fmt.Println(loc.Name)
+	}
+
 	return nil
 }
