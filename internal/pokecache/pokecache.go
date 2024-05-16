c73ee8617c5cdf26e9a02c15fc9d7b76a031c5d7
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-05-16 20:19:55 +0100

    added functions to delete items off cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -44,3 +44,22 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 
 	return val.val, ok
 }
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, las time.Duration) {
+
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for k, v := range c.cache {
+		if v.CreatedAt.Before(now.Add(-las)) {
+			delete(c.cache, k)
+		}
+	}
+}
