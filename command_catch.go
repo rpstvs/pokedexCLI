e14ea9d5b69a4de4217ca35aaec314753541d3ec
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-05-20 20:03:44 +0100

    continued catch feature

--- command_catch.go
+++ command_catch.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("expected a pokemon name")
+	}
+
+	pokemonName := args[0]
+
+	catched, err := cfg.pokeapiClient.CatchPokemon(pokemonName)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Trying to catch %s ...\n", pokemonName)
+	if catched {
+		fmt.Printf("Congratulations you've catched a Pokemon\n")
+	} else {
+		fmt.Printf("%s got away!! Try again", pokemonName)
+	}
 
 	return nil
 }
--- internal/pokeapi/pokemon_catch.go
+++ internal/pokeapi/pokemon_catch.go
@@ -1,6 +1,42 @@
 package pokeapi
 
-func (c *Client) catchPokemon(pokemonName string) (bool, error) {
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) CatchPokemon(pokemonName string) (bool, error) {
+
+	url := baseURL + "/pokemon/" + pokemonName
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return false, err
+	}
+
+	pokemon := Pokemon{}
+
+	err = json.Unmarshal(dat, &pokemon)
+
+	if err != nil {
+		return false, err
+	}
 
 	return true, nil
 }
